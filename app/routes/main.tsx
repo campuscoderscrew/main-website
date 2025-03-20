86ea45943e4915fc59237b0df3169069c3395b70
Franco Molinari <fmolinari106@gmail.com>
2025-03-20 18:02:22 -0400

    Update about section and contact section

--- app/routes/main.tsx
+++ app/routes/main.tsx
@@ -11,32 +11,31 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Main() {
   return (
-    <>
-      <div className="min-h-screen bg-[#FFF2F2]">
+    <div className='h-full w-full overflow-y-scroll fixed overflow-x-hidden'>
+      <div className="h-3/5 w-full bg-[#FFF2F2]">
       <Header />
-      
-      <main className="pt-20">
-        <section id="about" className="container mx-auto px-2 py-16">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="p-6 w-1/2">
-              <h2 className="font-bold mb-4 text-center text-6xl font-notable gap-y-2" style={{color: "#7886C7"}}>Campus Coders Crew</h2>
+        <main className="pt-20">
+          <section id="about" className="container mx-auto px-2 py-16">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-7">
+              <div className="p-6 w-1/2">
+                <h2 className="font-bold mb-4 text-center text-8xl font-notable gap-y-10" style={{color: "#7886C7"}}>Campus Coders Crew</h2>
+              </div>
+              <div className="flex justify-center items-center p-6 w-4/5">
+                <p className="text-black text-center text-4xl font-neuton">
+                  Empowering every aspiring student to collaborate, innovate, and grow in a flexible, inclusive, and agile community
+                </p>
+              </div>
             </div>
-            <div className="flex justify-center items-center p-6">
-              <p className="text-lg text-black text-center">
-                Empowering every aspiring student to collaborate, innovate, and grow in a flexible, inclusive, and agile community
+            <div className="text-center mt-12">
+              <p className="text-3xl font-bold font-neuton" style={{color: "#2D336B"}}>
+                We build websites for club. By Students. For Students.
               </p>
             </div>
-          </div>
-          <div className="text-center mt-12">
-            <p className="text-xl font-semibold" style={{color: "#2D336B"}}>
-              We build websites for club. By Students. For Students.
-            </p>
-          </div>
-        </section>
-      </main>
+          </section>
+        </main>
       </div>
+      <div className="h-2/3 bg-[#FFF2F2]"/>
       <Footer />
-    </>
-    
+    </div>
   );
 }
