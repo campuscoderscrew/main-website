f5d1fd76d799f23ed5439af7cff1e56027bb44b8
brthhule <brennenle@yahoo.com>
2025-03-20 17:07:38 -0400

    worked on incorporating campus_coders_crew_icon.png

--- app/welcome/navbar.tsx
+++ app/welcome/navbar.tsx
@@ -1,7 +1,9 @@
+import CCC_icon from "../assets/campus_coders_crew_icon.png"
+
 export function Navbar() {
     return (
         <div className="w-100% bg-[#2D336B] h-15">
-            Hello
+            <img src="CCC_icon"></img>
         </div>
     )
 }
