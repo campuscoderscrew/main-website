0761128530653db91d3ceb4c53be30de553bd163
brthhule <brennenle@yahoo.com>
2025-04-28 00:48:23 -0400

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   plugins: [tailwindcss(), tsconfigPaths()],
+  base: '/'
 });
 
 /**
